72c694bf90b9f4b8cde9ccac2c4529a1b6f86c45
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2022-11-01 12:48:08 +0900

    장태원
    과제 제출합니다.

--- src/main/java/onboarding/Problem7.java
+++ src/main/java/onboarding/Problem7.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Problem7 {
+
     public static List<String> solution(String user, List<List<String>> friends, List<String> visitors) {
         List<String> answer = Collections.emptyList();
         return answer;
