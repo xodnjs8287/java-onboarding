521d67c3ab8051e905cd696fc914b01d9fe2ad40
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2022-11-01 14:18:26 +0900

    Problem3
    Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,7 +2,21 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            String s = String.valueOf(i);
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == '3' || s.charAt(j) == '6' || s.charAt(j) == '9') {
+                    count++;
+                }
+            }
+
+        }
+        return count;
     }
+
+
+
 }
+
