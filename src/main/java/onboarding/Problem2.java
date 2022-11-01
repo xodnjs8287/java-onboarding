ae4bac6d5a8148c7228e67c360f9ce62ea9b08b7
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2022-11-01 14:12:55 +0900

    Revert "[온보딩] 장태원 미션 제출합니다."
    This reverts commit 6c8c81b7694cfd59d0d5b227970a983ced7db467.

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,30 +1,8 @@
 package onboarding;
 
-import java.util.Stack;
-
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String s1 = "";
-        Stack<Character> stack = new Stack<>();
-        char[] s_arr = cryptogram.toCharArray();
-
-        for (char c : s_arr) {
-            if (stack.empty()) {
-                stack.push(c);
-            } else {
-                if (stack.peek() == c) {
-                    stack.pop();
-                } else {
-                    stack.push(c);
-                }
-            }
-        }
-
-        for (Character character : stack) {
-            s1+=character;
-        }
-      return s1;
+        String answer = "answer";
+        return answer;
     }
-
-
 }
--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -2,21 +2,7 @@ package onboarding;
 
 public class Problem3 {
     public static int solution(int number) {
-        int count = 0;
-        for (int i = 1; i <= number; i++) {
-            String s = String.valueOf(i);
-
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == '3' || s.charAt(j) == '6' || s.charAt(j) == '9') {
-                    count++;
-                }
-            }
-
-        }
-        return count;
+        int answer = 0;
+        return answer;
     }
-
-
-
 }
-
