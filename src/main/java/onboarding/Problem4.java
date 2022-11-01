2d156f24685d2acafbb3866b2f9497af2f8ee599
xodnjs8287 <115204015+xodnjs8287@users.noreply.github.com>
2022-11-01 14:18:47 +0900

    Problem4
    Problem4

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -3,6 +3,27 @@ package onboarding;
 public class Problem4 {
     public static String solution(String word) {
         String answer = "";
+        char[] wordtemp = word.toCharArray();
+        int reverse, abstemp, ascii = 0;
+
+        for (int i = 0; i < wordtemp.length; i++) {
+            if (Character.isUpperCase(wordtemp[i])) {
+                ascii = 65;
+            } else if (Character.isLowerCase(wordtemp[i])) {
+                ascii = 97;
+
+            } else {
+                answer = answer + ' ';
+                continue;
+            }
+            abstemp = Math.abs(ascii - wordtemp[i]);
+            reverse = (ascii + 25) - abstemp;
+            answer = answer + String.valueOf((char) reverse);
+
+
+        }
         return answer;
     }
+
+
 }
